4ec2c562616bc6f486a52d39dbb0aec84d0878a3
James Thompson <james@plainprograms.com>
2013-05-10 00:23:37 -0600

    Added additional implementation for Privateer::Connection.new.

--- lib/privateer/connection.rb
+++ lib/privateer/connection.rb
@@ -3,5 +3,13 @@ class Privateer::Connection
     raise ArgumentError, 'store_name is required' if options[:store_name].nil?
     raise ArgumentError, 'api_key is required' if options[:api_key].nil?
     raise ArgumentError, 'password is required' if options[:password].nil?
+
+    @store_name = options[:store_name]
+    @api_key = options[:api_key]
+    @password = options[:password]
+
+    @store_url = "https:://#{@api_key}:#{@password}@#{@store_name}.myshopify.com/admin}"
+
+    @connection = Faraday.new(url: @store_url)
   end
 end
